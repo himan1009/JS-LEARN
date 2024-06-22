ff37cabdfb87c951d1787fa564962e39738f558c
himan1009 <shishusha922@gmail.com>
2024-06-22 23:23:21 +0530

    code02

---
+++ code02.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as new version
+
+
+// alert("hello")  // here we use node.js not browser
+
+console.log(3+3);
+console.log("Himanshu");
+
+let name="himan"
+let age=19
+let isLoggedIn=false
+
+// number=2 to power 53
+// bigint
+// string ""
+// boolean = true/false
+// null = standalone value (ye undefined nhi h khali h)
+// undefined
+// symbol
+// object
+
+console.log(typeof "himan");
+
+// object hai null
+console.log(typeof null);
+
+// undefined ye type h 
+console.log(typeof undefined)
