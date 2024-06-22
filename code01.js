0ae3370ef0b6d12d4afffd002ce346a64620d814
himan1009 <shishusha922@gmail.com>
2024-06-22 23:07:40 +0530

    code01

--- code01.js
+++ code01.js
@@ -1,1 +1,29 @@
 console.log("LEARN LET VAR CONST")
+
+
+const accountId=14453;
+let accountEmail="shishusha922@gmail.com"
+var accountPass="12345"
+accountCity="jaipur"
+let accountState
+// const ko change nhi kar skte h 
+
+// let ka value change ho skta h 
+accountEmail="hc@hc.com"
+console.log(accountEmail)
+
+// var v change krna possible h
+// not to use var 
+accountPass="212121"
+console.log(accountPass);
+
+// ye v print ho gya bina kuch ke v
+accountCity="bengal"
+console.log(accountCity);
+
+
+// ye undefined deta h q ki kuch value assigned hi nhi hua h
+console.log(accountState)
+
+// sbse bdhiya h let and const use kro 
+// but var ko almost use krna bnd kr do
