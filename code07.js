ad08377ae1f4220514febb069d2cf88315360f11
himan1009 <shishusha922@gmail.com>
2024-06-23 11:28:42 +0530

    code07

---
+++ code07.js
@@ -0,0 +1,21 @@
+// YAHA PE NORMAL COPY BAN JATA H
+let myname="himanshu kumar"
+let anothername=myname
+anothername="himan"
+
+console.log(myname)
+console.log(anothername)
+
+
+// YAHA PE REFERENCE KA GAME HUA
+let user={
+    email:"user@google.com",
+    upi:"abc.com",
+}
+
+let user2=user;
+
+user2.email="himan@google.com"
+
+console.log(user.email)
+console.log(user2.email)
