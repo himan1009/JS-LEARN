885259c97a0fa9e94a3c2624a7682fe2c4164056
himan1009 <shishusha922@gmail.com>
2024-06-23 11:01:59 +0530

    code04

---
+++ code04.js
@@ -0,0 +1,34 @@
+// opeartions
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+console.log(2+1)
+console.log(2*3)
+
+
+let str1="hello"
+let str2=" himan"
+console.log(str1+str2)
+
+// ek string or number ko add nhi karna h
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+
+// not good way to do so
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num1)
+
+
+// learn prefix and postfix
+let gameCounter=100
+gameCounter++
+++gameCounter
+console.log(gameCounter)
